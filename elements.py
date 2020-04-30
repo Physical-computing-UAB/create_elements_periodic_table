0474a70b22ace3ee3feff9516016ec4416e38c05
fernando <fernando@fernando-imac.local>
2020-05-01 00:47:04 +0200

    Final version

--- elements.py
+++ elements.py
@@ -7,11 +7,11 @@ pygame.init()
 screen = pygame.display.set_mode((80,100))
 background = pygame.image.load("frame.png")
 
-font_number = pygame.font.Font('CallingCode-Regular.ttf', 12)
+font_number = pygame.font.Font('./ttf/CallingCode-Regular.ttf', 12)
 textY_number = 8
-font_symbol = pygame.font.Font('CallingCode-Regular.ttf', 40)
+font_symbol = pygame.font.Font('./ttf/CallingCode-Regular.ttf', 40)
 textY_symbol= 24
-font_mass = pygame.font.Font('CallingCode-Regular.ttf', 12)
+font_mass = pygame.font.Font('./ttf/CallingCode-Regular.ttf', 12)
 textX_mass = 25
 textY_mass= 76
 
@@ -31,7 +31,583 @@ ele = {
         "Name"   : "Litium",
         "Symbol" : "Li",
         "Mass"   : 6.941
+    },
+    4 : {
+        "Name"   : "Breyllium",
+        "Symbol" : "Be",
+        "Mass"   : 9.012
+    },
+    5 : {
+        "Name"   : "Boron",
+        "Symbol" : "B",
+        "Mass"   : 10.810
+    },
+    6 : {
+        "Name"   : "Carbon",
+        "Symbol" : "C",
+        "Mass"   : 12.011
+    },
+    7 : {
+        "Name"   : "Nitrogen",
+        "Symbol" : "N",
+        "Mass"   : 14.007
+    },
+    8 : {
+        "Name"   : "Oxygen",
+        "Symbol" : "O",
+        "Mass"   : 15.999
+    },
+    9 : {
+        "Name"   : "Fluorine",
+        "Symbol" : "F",
+        "Mass"   : 18.998
+    },
+    10 : {
+        "Name"   : "Neon",
+        "Symbol" : "Ne",
+        "Mass"   : 20.180
+    },
+    11 : {
+        "Name"   : "Sodium",
+        "Symbol" : "Na",
+        "Mass"   : 22.990
+    },
+    12 : {
+        "Name"   : "Magnesium",
+        "Symbol" : "Mg",
+        "Mass"   : 24.305
+    },
+    13 : {
+        "Name"   : "Aluminium",
+        "Symbol" : "Al",
+        "Mass"   : 26.982
+    },
+    14 : {
+        "Name"   : "Silicon",
+        "Symbol" : "Si",
+        "Mass"   : 28.085
+    },
+    15 : {
+        "Name"   : "Phosphorus",
+        "Symbol" : "P",
+        "Mass"   : 30.974
+    },
+    16 : {
+        "Name"   : "Sulphur",
+        "Symbol" : "S",
+        "Mass"   : 32.060
+    },
+    17 : {
+        "Name"   : "Chlorine",
+        "Symbol" : "Cl",
+        "Mass"   : 32.450
+    },
+    18 : {
+        "Name"   : "Argon",
+        "Symbol" : "Ar",
+        "Mass"   : 39.948
+    },
+    19 : {
+        "Name"   : "Potassium",
+        "Symbol" : "K",
+        "Mass"   : 39.089
+    },
+    20 : {
+        "Name"   : "Calcium",
+        "Symbol" : "Ca",
+        "Mass"   : 40.078
+    },
+    21 : {
+        "Name"   : "Scandium",
+        "Symbol" : "Sc",
+        "Mass"   : 44.956
+    },
+    22 : {
+        "Name"   : "Titanium",
+        "Symbol" : "Ti",
+        "Mass"   : 47.867
+    },
+    23 : {
+        "Name"   : "Vanadium",
+        "Symbol" : "V",
+        "Mass"   : 50.942
+    },
+    24 : {
+        "Name"   : "Chromiun",
+        "Symbol" : "Cr",
+        "Mass"   : 51.996
+    },
+    25 : {
+        "Name"   : "Manganese",
+        "Symbol" : "Mn",
+        "Mass"   : 54.938
+    },
+    26 : {
+        "Name"   : "Iron",
+        "Symbol" : "Fe",
+        "Mass"   : 55.845
+    },
+    27 : {
+        "Name"   : "Cobalt",
+        "Symbol" : "Co",
+        "Mass"   : 58.933
+    },
+    28 : {
+        "Name"   : "Nickel",
+        "Symbol" : "Ni",
+        "Mass"   : 58.693
+    },
+    29 : {
+        "Name"   : "Copper",
+        "Symbol" : "Cu",
+        "Mass"   : 63.546
+    },
+    30 : {
+        "Name"   : "Zinc",
+        "Symbol" : "Zn",
+        "Mass"   : 65.380
+    },
+    31: {
+        "Name"   : "Gallium",
+        "Symbol" : "Ga",
+        "Mass"   : 69.723
+    },
+    32 : {
+        "Name"   : "Germanim",
+        "Symbol" : "Ge",
+        "Mass"   : 72.640
+    },
+    33 : {
+        "Name"   : "Arsenic",
+        "Symbol" : "As",
+        "Mass"   : 74.922
+    },
+    34 : {
+        "Name"   : "Selenium",
+        "Symbol" : "Se",
+        "Mass"   : 78.971
+    },
+    35 : {
+        "Name"   : "Bromine",
+        "Symbol" : "Br",
+        "Mass"   : 79.904
+    },
+    36 : {
+        "Name"   : "Krypton",
+        "Symbol" : "Kr",
+        "Mass"   : 83.798
+    },
+    37 : {
+        "Name"   : "Rubidium",
+        "Symbol" : "Rb",
+        "Mass"   : 85.468
+    },
+    38 : {
+        "Name"   : "Strontium",
+        "Symbol" : "Sr",
+        "Mass"   : 87.620
+    },
+    39 : {
+        "Name"   : "Yttrium",
+        "Symbol" : "Y",
+        "Mass"   : 88.906
+    },
+    40 : {
+        "Name"   : "Zirconium",
+        "Symbol" : "Zr",
+        "Mass"   : 91.224
+    },
+    41 : {
+        "Name"   : "Niobium",
+        "Symbol" : "Nb",
+        "Mass"   : 92.906
+    },
+    42 : {
+        "Name"   : "Molybdenum",
+        "Symbol" : "Mo",
+        "Mass"   : 95.950
+    },
+    43 : {
+        "Name"   : "Technetium",
+        "Symbol" : "Tc",
+        "Mass"   : 98.000
+    },
+    44 : {
+        "Name"   : "Ruthenium",
+        "Symbol" : "Ru",
+        "Mass"   : 101.070
+    },
+    45 : {
+        "Name"   : "Rhodium",
+        "Symbol" : "Rh",
+        "Mass"   : 102.91
+    },
+    46 : {
+        "Name"   : "Palladium",
+        "Symbol" : "Pd",
+        "Mass"   : 106.42
+    },
+    47 : {
+        "Name"   : "Silver",
+        "Symbol" : "Ag",
+        "Mass"   : 107.87
+    },
+    48 : {
+        "Name"   : "Cadmium",
+        "Symbol" : "Cd",
+        "Mass"   : 112.410
+    },
+    49 : {
+        "Name"   : "Indium",
+        "Symbol" : "In",
+        "Mass"   : 114.820
+    },
+    50 : {
+        "Name"   : "Tin",
+        "Symbol" : "Sn",
+        "Mass"   : 118.71
+    },
+    51 : {
+        "Name"   : "Antomony",
+        "Symbol" : "Sb",
+        "Mass"   : 121.76
+    },
+    52 : {
+        "Name"   : "Tellurium",
+        "Symbol" : "Te",
+        "Mass"   : 127.600
+    },
+    53 : {
+        "Name"   : "Iodine",
+        "Symbol" : "I",
+        "Mass"   : 126.900
+    },
+    54 : {
+        "Name"   : "Xenon",
+        "Symbol" : "Xe",
+        "Mass"   : 131.290
+    },
+    55 : {
+        "Name"   : "Caesium",
+        "Symbol" : "Cs",
+        "Mass"   : 132.91
+    },
+    56 : {
+        "Name"   : "Barium",
+        "Symbol" : "Ba",
+        "Mass"   : 137.330
+    },
+    57 : {
+        "Name"   : "Lanthanum",
+        "Symbol" : "La",
+        "Mass"   : 138.910
+    },
+    58 : {
+        "Name"   : "Cerium",
+        "Symbol" : "Ce",
+        "Mass"   : 140.120
+    },
+    59 : {
+        "Name"   : "Praeseodymium",
+        "Symbol" : "Pr",
+        "Mass"   : 140.910
+    },
+    60 : {
+        "Name"   : "Neodymium",
+        "Symbol" : "Nd",
+        "Mass"   : 144.240
+    },
+    61 : {
+        "Name"   : "Promethium",
+        "Symbol" : "Pm",
+        "Mass"   : 145.000
+    },
+    62 : {
+        "Name"   : "Samarium",
+        "Symbol" : "Sm",
+        "Mass"   : 150.360
+    },
+    63 : {
+        "Name"   : "Europium",
+        "Symbol" : "Eu",
+        "Mass"   : 151.960
+    },
+    64 : {
+        "Name"   : "Gadolinium",
+        "Symbol" : "Gd",
+        "Mass"   : 157.250
+    },
+    65 : {
+        "Name"   : "Terbium",
+        "Symbol" : "Tb",
+        "Mass"   : 158.930
+    },
+    66 : {
+        "Name"   : "Dysprosium",
+        "Symbol" : "Dy",
+        "Mass"   : 162.500
+    },
+    67 : {
+        "Name"   : "Holmium",
+        "Symbol" : "Ho",
+        "Mass"   : 164.930
+    },
+    68 : {
+        "Name"   : "Erbium",
+        "Symbol" : "Er",
+        "Mass"   : 167.260
+    },
+    69 : {
+        "Name"   : "Thullium",
+        "Symbol" : "Tm",
+        "Mass"   : 168.930
+    },
+    70 : {
+        "Name"   : "Ytterbium",
+        "Symbol" : "Yb",
+        "Mass"   : 173.050
+    },
+    71 : {
+        "Name"   : "Lutetium",
+        "Symbol" : "Lu",
+        "Mass"   : 174.970
+    },
+    72 : {
+        "Name"   : "Hafnium",
+        "Symbol" : "Hf",
+        "Mass"   : 178.490
+    },
+    73 : {
+        "Name"   : "Tantalum",
+        "Symbol" : "Ta",
+        "Mass"   : 180.950
+    },
+    74 : {
+        "Name"   : "Tungsten",
+        "Symbol" : "W",
+        "Mass"   : 183.840
+    },
+    75 : {
+        "Name"   : "Rhenium",
+        "Symbol" : "Re",
+        "Mass"   : 186.210
+    },
+    76 : {
+        "Name"   : "Osmium",
+        "Symbol" : "Os",
+        "Mass"   : 190.230
+    },
+    77 : {
+        "Name"   : "Iridium",
+        "Symbol" : "Ir",
+        "Mass"   : 192.220
+    },
+    78 : {
+        "Name"   : "Platinum",
+        "Symbol" : "Pt",
+        "Mass"   : 195.050
+    },
+    79 : {
+        "Name"   : "Gold",
+        "Symbol" : "Au",
+        "Mass"   : 196.970
+    },
+    80 : {
+        "Name"   : "Mercury",
+        "Symbol" : "Hg",
+        "Mass"   : 200.590
+    },
+    81 : {
+        "Name"   : "Thallium",
+        "Symbol" : "Th",
+        "Mass"   : 204.380
+    },
+    82 : {
+        "Name"   : "Lead",
+        "Symbol" : "Pb",
+        "Mass"   : 207.200
+    },
+    83 : {
+        "Name"   : "Bismuth",
+        "Symbol" : "Bi",
+        "Mass"   : 208.980
+    },
+    84 : {
+        "Name"   : "Polonium",
+        "Symbol" : "Po",
+        "Mass"   : 209.000
+    },
+    85 : {
+        "Name"   : "Astatine",
+        "Symbol" : "At",
+        "Mass"   : 210.000
+    },
+    86 : {
+        "Name"   : "Radon",
+        "Symbol" : "Rn",
+        "Mass"   : 222.000
+    },
+    87 : {
+        "Name"   : "Francium",
+        "Symbol" : "Fr",
+        "Mass"   : 223.000
+    },
+    88 : {
+        "Name"   : "Radium",
+        "Symbol" : "Ra",
+        "Mass"   : 226.000
+    },
+    89 : {
+        "Name"   : "Actinium",
+        "Symbol" : "Ac",
+        "Mass"   : 227.000
+    },
+    90 : {
+        "Name"   : "Thorium",
+        "Symbol" : "Th",
+        "Mass"   : 232.040
+    },
+    91 : {
+        "Name"   : "Protactinium",
+        "Symbol" : "Pa",
+        "Mass"   : 231.040
+    },
+    92 : {
+        "Name"   : "Uranium",
+        "Symbol" : "U",
+        "Mass"   : 238.030
+    },
+    93 : {
+        "Name"   : "Neptunium",
+        "Symbol" : "Np",
+        "Mass"   : 237.000
+    },
+    94 : {
+        "Name"   : "Plutonium",
+        "Symbol" : "Pu",
+        "Mass"   : 244.000
+    },
+    95 : {
+        "Name"   : "Americium",
+        "Symbol" : "Am",
+        "Mass"   : 243.000
+    },
+    96 : {
+        "Name"   : "Curium",
+        "Symbol" : "Cm",
+        "Mass"   : 247.000
+    },
+    97 : {
+        "Name"   : "Berkelium",
+        "Symbol" : "Bk",
+        "Mass"   : 247.000
+    },
+    98 : {
+        "Name"   : "Californium",
+        "Symbol" : "Cf",
+        "Mass"   : 251.000
+    },
+    99 : {
+        "Name"   : "Einstenium",
+        "Symbol" : "Es",
+        "Mass"   : 252.000
+    },
+    100 : {
+        "Name"   : "Fermium",
+        "Symbol" : "Fm",
+        "Mass"   : 257.000
+    },
+    101 : {
+        "Name"   : "Mendelevium",
+        "Symbol" : "Md",
+        "Mass"   : 258.000
+    },
+    102 : {
+        "Name"   : "Nobelium",
+        "Symbol" : "No",
+        "Mass"   : 259.000
+    },
+    103 : {
+        "Name"   : "Lawrencium",
+        "Symbol" : "Lr",
+        "Mass"   : 262.000
+    },
+    104 : {
+        "Name"   : "Rutherfordium",
+        "Symbol" : "Rf",
+        "Mass"   : 267.000
+    },
+    105 : {
+        "Name"   : "Dubnium",
+        "Symbol" : "Db",
+        "Mass"   : 268.000
+    },
+    106 : {
+        "Name"   : "Seaborgium",
+        "Symbol" : "Sg",
+        "Mass"   : 271.000
+    },
+    107 : {
+        "Name"   : "Bohrium",
+        "Symbol" : "Bh",
+        "Mass"   : 272.000
+    },
+    108 : {
+        "Name"   : "Hassium",
+        "Symbol" : "Hs",
+        "Mass"   : 277.000
+    },
+    109 : {
+        "Name"   : "Meitnerium",
+        "Symbol" : "Mt",
+        "Mass"   : 276
+    },
+    110 : {
+        "Name"   : "Darsmtadtium",
+        "Symbol" : "Ds",
+        "Mass"   : 281.000
+    },
+    111 : {
+        "Name"   : "Roentgenium",
+        "Symbol" : "Rg",
+        "Mass"   : 280.000
+    },
+    112 : {
+        "Name"   : "Copernicium",
+        "Symbol" : "Cn",
+        "Mass"   : 285.000
+    },
+    113 : {
+        "Name"   : "Nihonium",
+        "Symbol" : "Nh",
+        "Mass"   : 285.000
+    },
+    114 : {
+        "Name"   : "Flerovium",
+        "Symbol" : "Fl",
+        "Mass"   : 287.000
+    },
+    115 : {
+        "Name"   : "Moscovium",
+        "Symbol" : "Mc",
+        "Mass"   : 289.000
+    },
+    116 : {
+        "Name"   : "Livermorium",
+        "Symbol" : "Lv",
+        "Mass"   : 291.000
+    },
+    117 : {
+        "Name"   : "Tenessine",
+        "Symbol" : "Ts",
+        "Mass"   : 294.000
+    },
+    118 : {
+        "Name"   : "Oganesson",
+        "Symbol" : "Og",
+        "Mass"   : 294.000
     }
+
 }
 num_ele = len(ele)
 
@@ -56,27 +632,42 @@ while running:
         screen.fill((255,255,255))
         screen.blit(background,(0,0))
 
-        if i<10:
+        if (i+1)<10:
             textX_number = 39
-        elif i<100:
-            textX_number = 30
+        elif (i+1)<100:
+            textX_number = 35
         else:
-            textX_number = 25
+            textX_number = 31
 
         # print((ele[i+1]["Name"]))
 
-        if len(ele[i+1]["Symbol"]) < 2:
+
+
+        text_number_str  = "{}".format(i+1)
+        textX_symbol_str = ele[i+1]["Symbol"]
+        textX_mass_str   = "{:.3f}".format(ele[i+1]["Mass"])
+
+        if len(textX_symbol_str) < 2:
             textX_symbol = 30
         else:
             textX_symbol = 19
 
-        print_text(textX_number, textY_number, font_number, str(i+1))
-        print_text(textX_symbol, textY_symbol, font_symbol, ele[i+1]["Symbol"])
-        print_text(textX_mass, textY_mass, font_mass, str(ele[i+1]["Mass"]))
+        print(textX_mass_str)
+        if len(textX_mass_str) < 6:
+            textX_mass = 23
+        elif len(textX_mass_str) < 7:
+            textX_mass = 20
+        else:
+            textX_mass = 16
+
+
+        print_text(textX_number, textY_number, font_number, text_number_str)
+        print_text(textX_symbol, textY_symbol, font_symbol, textX_symbol_str)
+        print_text(textX_mass, textY_mass, font_mass, textX_mass_str)
         pygame.display.update()
 
         filename = "./imgs/{}.jpg".format(i+1)
         pygame.image.save(screen, filename)
-        time.sleep(2)
+        time.sleep(.2)
 
         running = False
